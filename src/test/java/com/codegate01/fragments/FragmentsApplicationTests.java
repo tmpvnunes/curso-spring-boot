7506e76afd5dc0dc468c14d821fbc8d7fd449187
Tiago Nunes <cceatmp@ucl.ac.uk>
2021-01-16 20:35:48 +0000

    hello world

---
+++ src/test/java/com/codegate01/fragments/FragmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegate01.fragments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
